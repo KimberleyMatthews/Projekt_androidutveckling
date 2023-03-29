d55082186325b7e27fd61bf6c1675367c5866c4a
Kimberley <kimberleyahnlundmatthews@gmail.com>
2023-03-29 16:04:31 +0200

    Firebase - Setup for Firebase database done

--- app/src/main/java/com/example/projekt_androidutveckling/SignInFragment.kt
+++ app/src/main/java/com/example/projekt_androidutveckling/SignInFragment.kt
@@ -6,17 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.projekt_androidutveckling.databinding.FragmentSignInBinding
-
 class SignInFragment : Fragment() {
 
     // SetUp Binding
     private lateinit var binding: FragmentSignInBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,7 +23,19 @@ class SignInFragment : Fragment() {
 
         val view = binding.root
 
+        // ID's
+        val etUserName = binding.etUsername
+        val etUserPassword = binding.etUserPassword
         val btnSignIn = binding.btnSignIn
+
+        var listOfUsers = arrayListOf<Users>()
+
+        // OnClick - Check name and password
+        btnSignIn.setOnClickListener(){
+
+            val userName = etUserName.text.toString()
+            val userPassword = etUserPassword.text.toString()
+        }
 
         return view
 
---
+++ app/src/main/java/com/example/projekt_androidutveckling/Users.kt
@@ -0,0 +1,9 @@
+package com.example.projekt_androidutveckling
+
+data class Users(
+    val username: String,
+    val password: String,
+    val isRegistered: Boolean,
+){
+
+}
