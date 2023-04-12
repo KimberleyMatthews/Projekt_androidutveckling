58dd569a43f123855657f95131d8439e07406733
Kimberley <kimberleyahnlundmatthews@gmail.com>
2023-04-12 13:50:16 +0200

    GameFragment - finished last codes for the game AboutFragment - Changed btn_home to ImageButton and designs TODO - connect Game with LogIn using Navigate

--- app/src/main/java/com/example/projekt_androidutveckling/GameFragment.kt
+++ app/src/main/java/com/example/projekt_androidutveckling/GameFragment.kt
@@ -1,59 +1,157 @@
 package com.example.projekt_androidutveckling
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import com.example.projekt_androidutveckling.databinding.FragmentInloggedBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class GameFragment : Fragment()
+{
+    enum class Turn {
+        NOUGHT,
+        CROSS
+    }
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class GameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var firstTurn = Turn.CROSS
+    private var currentTurn = Turn.CROSS
+
+    private var crossesScore = 0
+    private var noughtsScore = 0
+
+    private var boardList = mutableListOf<Button>()
+
+    private lateinit var binding : FragmentInloggedBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        binding = FragmentInloggedBinding.inflate(layoutInflater)
+        (binding.root)
+        initBoard()
+    }
+
+    private fun initBoard() {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+    }
+    fun boardTapped(view: View) {
+        if(view !is Button)
+            return
+        addToBoard(view)
+
+        // Check if someone won
+        if(checkForVictory(NOUGHT)) {
+            //add score to scoreboard
+            noughtsScore++
+            result("Noughts Win!")
+        }
+        if(checkForVictory(CROSS)) {
+            //add score to scoreboard
+            crossesScore++
+            result("Crosses Win!")
+        }
+
+        if(fullBoard()) {
+            result("Draw")
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    // Check for Victory
+    private fun checkForVictory(s: String): Boolean {
+        // Horizontal Victory
+        if(match(binding.a1,s) && match(binding.a2,s) && match(binding.a3,s))
+            return true
+        if(match(binding.b1,s) && match(binding.b2,s) && match(binding.b3,s))
+            return true
+        if(match(binding.c1,s) && match(binding.c2,s) && match(binding.c3,s))
+            return true
+
+        // Vertical Victory
+        if(match(binding.a1,s) && match(binding.b1,s) && match(binding.c1,s))
+            return true
+        if(match(binding.a2,s) && match(binding.b2,s) && match(binding.c2,s))
+            return true
+        if(match(binding.a3,s) && match(binding.b3,s) && match(binding.c3,s))
+            return true
+
+        // Diagonal Victory
+        if(match(binding.a1,s) && match(binding.b2,s) && match(binding.c3,s))
+            return true
+        if(match(binding.a3,s) && match(binding.b2,s) && match(binding.c1,s))
+            return true
+
+        return false
+    }
+    private fun match(button: Button, symbol: String) = button.text == symbol
+    private fun result(title: String) {
+        val message = "\nNoughts $noughtsScore\n\nCrosses $crossesScore"
+        AlertDialog.Builder(requireContext())
+            .setTitle(title)
+            .setPositiveButton("Reset")
+            { _,_ ->
+                resetBoard()
+            }
+            .setCancelable(false)
+            .show()
+    }
+    private fun resetBoard() {
+        for(button in boardList) {
+            button.text = ""
+        }
+        if(firstTurn == Turn.NOUGHT)
+            firstTurn = Turn.CROSS
+        else if(firstTurn == Turn.CROSS)
+            firstTurn = Turn.NOUGHT
+
+        currentTurn = firstTurn
+        setTurnLabel()
+    }
+    private fun fullBoard(): Boolean {
+
+        for(button in boardList) {
+            if(button.text == "")
+                return false
+        }
+        return true
+    }
+
+    private fun addToBoard(button: Button) {
+        if(button.text != "")
+            return
+
+        if(currentTurn == Turn.NOUGHT) {
+            button.text = "NOUGHT"
+            currentTurn = Turn.CROSS
+        }
+        else if(currentTurn == Turn.CROSS) {
+            button.text = "CROSS"
+            currentTurn = Turn.NOUGHT
+        }
+       setTurnLabel()
+    }
+
+    private fun setTurnLabel() {
+        var turnText = ""
+        if (currentTurn == Turn.CROSS)
+            turnText = "Turn $CROSS"
+        else if (currentTurn == Turn.NOUGHT)
+            turnText = "Turn $NOUGHT"
+
+        binding.tvTurn.text = turnText
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val NOUGHT = "0"
+        const val CROSS = "X"
     }
+
 }
